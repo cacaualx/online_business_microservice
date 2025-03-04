a4570a4d1276ef1811141d427f0dcceb8c6b0de6
cauealx <cauealexandre2002@gmail.com>
2025-03-04 13:30:59 -0300

    init commit

--- src/main/java/com/productmicroservice/Product_MS/controller/ProductController.java
+++ src/main/java/com/productmicroservice/Product_MS/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.productmicroservice.Product_MS.controller;
 
 import com.productmicroservice.Product_MS.entity.Product;
 import com.productmicroservice.Product_MS.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -13,39 +16,73 @@ public class ProductController {
     private ProductService service;
 
     //Metodo POST
+    @Operation(description = "Adiciona um produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Adiciona um novo produto ao banco de dados"),
+            @ApiResponse(responseCode = "500", description = "Erro de conexão")
+    })
     @PostMapping("/addProduct")
     public Product addProduct(@RequestBody Product product){
         return service.saveProduct(product);
     }
 
+    @Operation(description = "Adiciona uma lsita de produtos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Adiciona uma lista de produtos"),
+            @ApiResponse(responseCode = "500", description = "Erro de conexão")
+    })
     @PostMapping("addProducts")
     public List<Product> addProducts(@RequestBody List<Product> products) {
         return service.saveProducts(products);
     }
 
     //Metodo GET
+    @Operation(description = "Retorna uma lsita de produtos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna uma lsita de produtos"),
+    })
     @GetMapping("/products")
     public List<Product> findAllProducts(){
         return service.getProducts();
     }
 
+    @Operation(description = "Busca um produto pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o produto que tem o respectivo id"),
+            @ApiResponse(responseCode = "400", description = "Produto não encontrado")
+    })
     @GetMapping("/product/{id}")
     public Product findProductById(@PathVariable int id) {
         return service.getProductById(id);
     }
 
+    @Operation(description = "Busca um produto pelo nome")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o produto que tem o respectivo nome"),
+            @ApiResponse(responseCode = "400", description = "Produto não encontrado")
+    })
     @GetMapping("/product/{name}")
     public Product findProductByName(@PathVariable String name) {
         return service.getProductByName(name);
     }
 
     //Metodo PUT
+    @Operation(description = "Atualiza um produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto atualizado"),
+            @ApiResponse(responseCode = "400", description = "Produto não encontrado")
+    })
     @PutMapping("/update")
     public Product updateProduct(@RequestBody Product product) {
         return service.updateProduct(product);
     }
 
     //Metodo DELETE
+    @Operation(description = "Deleta um produto pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto deletado"),
+            @ApiResponse(responseCode = "400", description = "Produto não encontrado")
+    })
     @DeleteMapping("/delete/{id}")
     public String deleteProduct(@PathVariable int id) {
         return service.deleteProduct(id);
